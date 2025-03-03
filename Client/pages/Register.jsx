1d0d23fcd947a8ad19600d8b1d0d26839afedccf
Shuvamdt <shuvamdt1230@gmail.com>
2025-03-03 21:28:34 +0530

    Updating responsiveness for register page

--- Client/pages/Register.jsx
+++ Client/pages/Register.jsx
@@ -7,6 +7,17 @@ import axios from "axios";
 const Register = (props) => {
   const [hover, setHover] = React.useState(props.state);
   const [selectedSection, setSelectedSection] = React.useState(props.page);
+  const [isSmallScreen, setIsSmallScreen] = React.useState(
+    window.innerWidth < 640
+  );
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 640);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const [loginData, setLoginData] = React.useState({ email: "", password: "" });
   const [registerData, setRegisterData] = React.useState({
@@ -76,63 +87,65 @@ const Register = (props) => {
   const loginStyle = { backgroundColor: "#aa60c8", opacity: 0.2 };
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="flex flex-col flex-grow justify-center items-center">
-        <div className="relative w-250 h-180 flex backdrop-blur border-1 border-purple-800 rounded-lg m-10 p-4">
-          <motion.div
-            className="w-109 h-164 m-4 p-4 absolute rounded-lg z-1"
-            style={loginStyle}
-            animate={{ x: hover ? 0 : 500 }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
-          />
-          <div
-            className="w-109 h-164 flex flex-col rounded-lg m-4 p-4 justify-center items-center login z-10 font"
-            onClick={() => handleChange("login")}
-            style={{ color: hover ? "#d69ade" : null }}
+    <div className="flex flex-col min-h-screen justify-center items-center">
+      <div className="m-10 relative w-full max-w-250 flex flex-col sm:flex-row sm:space-x-4 sm:items-center backdrop-blur border border-purple-800 rounded-lg p-6">
+        <motion.div
+          className="w-109 h-164 m-4 p-4 absolute rounded-lg z-1"
+          style={loginStyle}
+          animate={{
+            x: isSmallScreen ? 0 : hover ? 0 : 470,
+            y: isSmallScreen ? (hover ? 0 : 700) : 0,
+          }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        />
+        <div
+          className="w-109 h-164 flex flex-col rounded-lg m-4 p-4 justify-center items-center login z-10 font"
+          onClick={() => handleChange("login")}
+          style={{ color: hover ? "#d69ade" : null }}
+        >
+          <h3 style={{ fontSize: 30 }} className="p-5">
+            Welcome Back!
+          </h3>
+          <form
+            className="flex flex-col justify-center items-center"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleLogin();
+            }}
           >
-            <h3 style={{ fontSize: 30 }} className="p-5">
-              Welcome Back!
-            </h3>
-            <form
-              className="flex flex-col justify-center items-center"
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleLogin();
+            <div className="flex flex-col">
+              <label className="p-2 m-1">Email</label>
+              <input
+                type="email"
+                name="email"
+                required
+                style={{ backgroundColor: "#AA60C8", color: "white" }}
+                className="rounded-md m-1 p-2"
+                value={loginData.email}
+                onChange={handleLoginChange}
+              />
+              <label className="p-2 m-1">Password</label>
+              <input
+                type="password"
+                name="password"
+                required
+                style={{ backgroundColor: "#AA60C8", color: "white" }}
+                className="rounded-md m-1 p-2"
+                value={loginData.password}
+                onChange={handleLoginChange}
+              />
+            </div>
+            <button
+              className="rounded-lg py-2 px-4 m-4 font login"
+              style={{
+                backgroundColor: "#AA60C8",
+                color: "white",
               }}
             >
-              <div className="flex flex-col">
-                <label className="p-2 m-1">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  required
-                  style={{ backgroundColor: "#AA60C8", color: "white" }}
-                  className="rounded-md m-1 p-2"
-                  value={loginData.email}
-                  onChange={handleLoginChange}
-                />
-                <label className="p-2 m-1">Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  required
-                  style={{ backgroundColor: "#AA60C8", color: "white" }}
-                  className="rounded-md m-1 p-2"
-                  value={loginData.password}
-                  onChange={handleLoginChange}
-                />
-              </div>
-              <button
-                className="rounded-lg py-2 px-4 m-4 font login"
-                style={{
-                  backgroundColor: "#AA60C8",
-                  color: "white",
-                }}
-              >
-                Login
-              </button>
-            </form>
-            {/* <a
+              Login
+            </button>
+          </form>
+          {/* <a
               href="http://localhost:4000/auth/google/login"
               className="p-1 m-2 rounded-lg border border-purple-800 flex justify-center items-center mt-auto"
               style={{
@@ -143,68 +156,67 @@ const Register = (props) => {
               <img className="icon h-10 w-10 m-2" src={google} alt="logo" />
               <p>Sign in With Your Google Account</p>
             </a> */}
-          </div>
-          <div className="h-164 m-4 rounded-full border-1 border-purple-800 z-10"></div>
-          <div
-            className="w-109 h-164 flex flex-col rounded-lg m-4 p-4 justify-center items-center login z-10 font"
-            onClick={() => handleChange("signup")}
-            style={{
-              color: !hover ? "#d69ade" : null,
+        </div>
+        <div
+          className="w-109 h-164 flex flex-col rounded-lg m-4 p-4 justify-center items-center login z-10 font"
+          onClick={() => handleChange("signup")}
+          style={{
+            color: !hover ? "#d69ade" : null,
+          }}
+        >
+          <h3 style={{ fontSize: 30 }} className="p-5">
+            Join us Today!
+          </h3>
+          <form
+            className="flex flex-col justify-center items-center"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSignup();
             }}
           >
-            <h3 style={{ fontSize: 30 }} className="p-5">
-              Join us Today!
-            </h3>
-            <form
-              className="flex flex-col justify-center items-center"
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleSignup();
+            <div className="flex flex-col">
+              <label className="p-2 m-1">Your Name</label>
+              <input
+                type="text"
+                name="name"
+                required
+                style={{ backgroundColor: "#AA60C8", color: "white" }}
+                className="rounded-md m-1 p-2"
+                value={registerData.name}
+                onChange={handleSignupChange}
+              />
+              <label className="p-2 m-1">Email</label>
+              <input
+                type="text"
+                name="email"
+                required
+                style={{ backgroundColor: "#AA60C8", color: "white" }}
+                className="rounded-md m-1 p-2"
+                value={registerData.email}
+                onChange={handleSignupChange}
+              />
+              <label className="p-2 m-1">Password</label>
+              <input
+                type="password"
+                name="password"
+                minLength="6"
+                required
+                style={{ backgroundColor: "#AA60C8", color: "white" }}
+                className="rounded-md m-1 p-2"
+                value={registerData.password}
+                onChange={handleSignupChange}
+              />
+            </div>
+            <button
+              className="rounded-lg py-2 px-4 m-4 font login"
+              style={{
+                backgroundColor: "#AA60C8",
               }}
             >
-              <div className="flex flex-col">
-                <label className="p-2 m-1">Your Name</label>
-                <input
-                  type="text"
-                  name="name"
-                  required
-                  style={{ backgroundColor: "#AA60C8", color: "white" }}
-                  className="rounded-md m-1 p-2"
-                  value={registerData.name}
-                  onChange={handleSignupChange}
-                />
-                <label className="p-2 m-1">Email</label>
-                <input
-                  type="text"
-                  name="email"
-                  required
-                  style={{ backgroundColor: "#AA60C8", color: "white" }}
-                  className="rounded-md m-1 p-2"
-                  value={registerData.email}
-                  onChange={handleSignupChange}
-                />
-                <label className="p-2 m-1">Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  minLength="6"
-                  required
-                  style={{ backgroundColor: "#AA60C8", color: "white" }}
-                  className="rounded-md m-1 p-2"
-                  value={registerData.password}
-                  onChange={handleSignupChange}
-                />
-              </div>
-              <button
-                className="rounded-lg py-2 px-4 m-4 font login"
-                style={{
-                  backgroundColor: "#AA60C8",
-                }}
-              >
-                Sign Up
-              </button>
-            </form>
-            {/* <a
+              Sign Up
+            </button>
+          </form>
+          {/* <a
               href="http://localhost:4000/auth/google"
               className="p-1 m-2 rounded-lg border border-purple-800 flex justify-center items-center mt-auto"
               style={{
@@ -215,7 +227,6 @@ const Register = (props) => {
               <img className="icon h-10 w-10 m-2" src={google} alt="logo" />
               <p>Sign up With Your Google Account</p>
             </a> */}
-          </div>
         </div>
       </div>
     </div>
