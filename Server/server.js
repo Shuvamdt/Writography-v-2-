58f6c5d5a8bab238791a16bbb412d1572fbebcfa
Shuvamdt <shuvamdt1230@gmail.com>
2025-03-05 00:43:12 +0530

    Deploying edits

--- Server/server.js
+++ Server/server.js
@@ -28,16 +28,7 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:4000",
-      "https://writography-frontend.vercel.app",
-    ],
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "*", credentials: true }));
 
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
